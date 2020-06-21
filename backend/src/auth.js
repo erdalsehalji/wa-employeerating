65ecb91581cc92d0d1828b7c55d9cd1b131911ae
erdalsehalji <58055108+erdalsehalji@users.noreply.github.com>
2020-06-21 23:25:26 +0200

    Add files via upload

---
+++ backend/src/auth.js
@@ -0,0 +1,84 @@
+import mongo from 'mongodb'
+import connect from './db.js'
+import bcrypt from 'bcrypt'
+import jwt, { verify } from 'jsonwebtoken'
+import mongose from 'mongoose'
+
+(async () => {
+  let db = await connect()
+  await db.collection("employees").createIndex({signupEmail: 1}, {unique: true})
+})()
+
+
+
+export default {
+  async  registerUser(userData) {
+        console.log("dodao :" ,userData)
+        let db = await connect()
+   /*      let salt = bcrypt.hash(userData.signupPassword, 8) */
+        let doc = {
+          signupEmail: userData.signupEmail,
+          signupSurname: userData.signupSurname,
+          signupPassword: await bcrypt.hash(userData.signupPassword,8),
+          signupEnterprise: userData.signupEnterprise,
+          signupPosition: userData.signupPosition,
+          signupAge: userData.signupAge,
+          signupSex: userData.signupSex,
+          signupEmail: userData.signupEmail,
+          signupAddress: userData.signupAddress,
+          signupCity: userData.signupCity,
+          signupType: userData.signupType,
+          signupImage: userData.signupImage
+        }
+
+        try {
+           let result = await db.collection("employees").insertOne(doc)
+           if (result && result.insertedId)
+           return result.insertedId
+        }
+        catch (e) {
+            if (e.name == 'MongoError' && e.code == 11000) {
+            throw new Error("Email already exists")
+          }
+        }
+    },
+    async authenticateUser(signupEmail, signupPassword) {
+      let db = await connect()
+      let user = await db.collection("employees").findOne({ signupEmail: signupEmail })  
+
+      if(user && user.signupPassword && (await bcrypt.compare(signupPassword, user.signupPassword))) {
+        delete user.signupPassword
+       
+        let token = jwt.sign(user, 'secretkey', {
+          algorithm: "HS512",
+          expiresIn: "1 week"
+        })
+        return {
+          token, 
+          signupEmail: user.signupEmail
+        }
+      }
+      else {
+        throw new Error("No user!")
+      }
+    },
+   
+    async verify(req, res, next) {
+      try {
+        let authorization = req.headers.authorization.split(' ')
+      let type = authorization[0]
+      let token = authorization[1]
+  
+      if(type !== "Bearer") {
+        return res.status(401).send()
+      }
+      else {
+          req.jwt = jwt.verify(token, 'secretkey')
+          return next()
+      }}
+      catch(e) {
+        return  res.status(401).send() 
+      }
+    }
+
+}
